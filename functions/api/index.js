ec33d79e02470eab23f9771c02f6ed80105acd71
literasibadguy <firasrafislam@live.com>
2024-02-05 08:12:00 +0700

    fix functions async await

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,9 +1,6 @@
 export function onRequest(context) {
     console.log(`Hello API ${context.params.post}`)
 
-    const response = await fetch(context.request)
-
-
     const data = {
         name: "Thanks for testing our API",
       };
@@ -17,30 +14,4 @@ export function onRequest(context) {
       });
 }
 
-export async function onRequestGet(context) {
-
-  const someHost = "https://www.tikwm.com/api/user/posts?unique_id=@fujiiian&hd=1";
-  const url = someHost + "unique_id=@fujiiian&hd=1";
-
-  async function gatherResponse(response) {
-      const { headers } = response;
-      const contentType = headers.get("content-type") || "";
-      if (contentType.includes("application/json")) {
-        return JSON.stringify(await response.json());
-      }
-      return response.text();
-    }
-
-  const init = {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-
-      },
-  };
-
-  const response = await fetch(url, init);
-  const results = await gatherResponse(response);
-  return new Response(results, init);
-
-}
 
